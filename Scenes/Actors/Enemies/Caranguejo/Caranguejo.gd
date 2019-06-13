9971e07b1309bbe2149343c23ae50165eec4c052
Henrique <henfinzim@gmail.com>
2019-06-12 22:00:47 -0300

    Anda certo, falta dano e vida

--- Scenes/Actors/Enemies/Caranguejo/Caranguejo.gd
+++ Scenes/Actors/Enemies/Caranguejo/Caranguejo.gd
@@ -7,14 +7,17 @@ var going = "right"
 var normal = Vector2(0,-1)
 var right = Vector2()
 var left = Vector2()
-var speed = 200
+var speed = 50
+var gravity = 600
 var velocity = Vector2()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
 	if randf()>0.5:
 		going = "left"
-	normal = normal.normalized()
+	normal = (normal.rotated(rotation)).normalized()
+	print(rotation)
+	print((normal.angle_to(Vector2(0,-1))))
 	right = normal.rotated(PI/2)
 	left = normal.rotated(-PI/2)
 	pass # Replace with function body.
@@ -25,6 +28,7 @@ func _process(delta):
 	$RayCastRightGround.force_raycast_update()
 	$RayCastLeftWall.force_raycast_update()
 	$RayCastRightWall.force_raycast_update()
+	#print($RayCastLeftWall.is_colliding()," ",$RayCastLeftGround.is_colliding()," ",$RayCastRightGround.is_colliding()," ",$RayCastRightWall.is_colliding())
 	if going == "left":
 		if $RayCastLeftWall.is_colliding() or not $RayCastLeftGround.is_colliding():
 			going = "right"
@@ -32,10 +36,13 @@ func _process(delta):
 		if $RayCastRightWall.is_colliding() or not $RayCastRightGround.is_colliding():
 			going = "left"
 		pass
-	if going == "left":
-		velocity = left*speed
-	elif going == "right":
-		velocity = right*speed
+	velocity -= normal*delta*gravity
 	if is_on_floor():
+		velocity = Vector2(0,0)
+		if going == "left":
+			velocity = left*speed
+		elif going == "right":
+			velocity = right*speed
 		pass
+	move_and_slide_with_snap(velocity,-0.1*normal,normal)
 	pass
